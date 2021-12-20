270946f99d14cf8a71b07c755c0b108423370ced
Dar-rius <mohamedtine17@gmail.com>
2021-12-20 00:59:54 +0000

    update analyse

--- tools.py
+++ tools.py
@@ -4,20 +4,88 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import numpy as np
 
-#A function allowing to convert two variables which contains 
-# the number of men and women according to a base age in float 
-# in int, to display the 5 1st elements of the column to then merge 
-# the columns and create a new dataframe for at the end make a graph
 
-# pour un seul pays
-def calPopMaleFemale(dataM, dataF, dataCountry, dataT):
-    print(dataCountry)
-    print("Graph de la population masculine et femine et mondiale:")
-    #merge series and display a graph
-    print("Data male and female: ")
-    dataTotal = pd.merge(dataM, dataF, on="Time", how='inner')
-    print(dataTotal)
-    print("Data sex total: ")
-    print(dataT)
-    sns.lineplot(data=dataTotal) 
-    sns.lineplot(data=dataT)
+#fonction permettant de calculer le taux des pop dans diff regions
+def calPopRegions(popO, popE, popS, popN, popC, popT, name):
+
+    #Calcul de la population du taux des populations dans 
+    #differentes regions,
+    tauxO = (100*popO)/popT
+    tauxE = (100*popE)/popT
+    tauxS = (100*popS)/popT
+    tauxN = (100*popN)/popT
+    tauxC = (100*popC)/popT
+
+    #Visualisation des donnees
+    fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+
+    recipe = ["population de l'ouest: %.2f" %tauxO,
+            "population de l'est: %.2f" %tauxE,
+            "population du sud: %.2f" %tauxS,
+            "population du nord: %.2f" %tauxN,
+            "population du centre: %.2f" %tauxC]
+
+    data = [tauxO, tauxE, tauxS, tauxN, tauxC]
+
+    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    kw = dict(arrowprops=dict(arrowstyle="-"),
+            bbox=bbox_props, zorder=0, va="center")
+
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        ax.annotate(recipe[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                    horizontalalignment=horizontalalignment, **kw)
+
+    ax.set_title(f"Croissance de la population dans les regions ({name})")
+
+    plt.show()
+
+
+#fonction permettant d'avoir le taux de la population totale dans chaque
+#continent et de le comparrer entre eux
+def continentTaux(popA, popB, popC, name ):
+    #Le taux de la population totale 
+    tauxM = (100*popA)/popC
+    tauxF = (100*popB)/popC
+    tauxT = [tauxM, tauxF]
+
+    fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+
+    recipe = ["Pop Male",
+            "Pop Female",]
+
+    data = [tauxT]
+    population = [x.split()[-1] for x in recipe]
+
+
+    def func(pct, allvals):
+        absolute = int(np.round(pct/100.*np.sum(allvals)))
+        return "{:.1f}%\n({:d} g)".format(pct, absolute)
+
+
+    wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
+                                    textprops=dict(color="w"))
+
+    ax.legend(wedges, population,
+            title="population",
+            loc="center left",
+            bbox_to_anchor=(1, 0, 0.5, 1))
+
+    plt.setp(autotexts, size=8, weight="bold")
+
+    ax.set_title(f"La taux de la pop male et femelle en {name}")
+
+    plt.show()
+
+
+
+#def continentTaux(popAfric, popAsia, popEurope, popOceanic, popNafta, popLatin):
+    #le taux de la population africaine
+    #tauxAfric = (100*popAfric)/
